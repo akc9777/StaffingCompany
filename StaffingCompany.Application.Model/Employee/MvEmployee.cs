a04424ac388ebfbe89a1512f0f618b8f4e2b4df6
Abhishek KC <akc9777@outlook.com>
2020-10-07 09:20:03 +0545

    Added Employee Model

---
+++ StaffingCompany.Application.Model/Employee/MvEmployee.cs
@@ -0,0 +1,16 @@
+﻿namespace StaffingCompany.Application.Model.Employee
+{
+    public class MvEmployee
+    {
+        public int EmployeeId { get; set; }
+        public int OrganizationId { get; set; }
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public int InsertPersonId { get; set; }
+    }
+}
